74e2746e66da827fcf58d2969b55d26428265bf3
Prince <xiuuscv@gmail.com>
2024-05-31 10:41:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP, should be 111 if stable
 sed -i 's/192.168.1.1/192.168.1.211/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OverPrinceWrt/g' package/base-files/files/bin/config_generate
