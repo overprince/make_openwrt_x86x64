dd6b03bfb2eb71dc62685c274bf404ed45defa2b
Prince <xiuuscv@gmail.com>
2024-11-09 21:23:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
 echo 'src-git lucipasswall https://github.com/xiaorouji/openwrt-passwall-packages;main' >>feeds.conf.default
 #echo 'src-git lucipasswall https://github.com/sbwml/openwrt_helloworld;go1.21' >>feeds.conf.default
